7e6831a7b53faab6543be727fcaae30167ac9726
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-06-06 21:58:05 -0700

    no c task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """
+    Removes all characters c and C from a string
+
+    Args:
+        my_string: string
+
+    Return:
+        String
+    """
+    return "".join([c if c not in "cC" else "" for c in list(my_string)]
