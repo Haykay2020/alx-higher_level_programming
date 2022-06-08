2dc2b22a287279f47bb5dfe53202707b13d2e2a6
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-06-08 04:49:06 -0700

    simple calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,8 +3,8 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    a = 20
-    b = 8
+    a = 10
+    b = 5
 
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
