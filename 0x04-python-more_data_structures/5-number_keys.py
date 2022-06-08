a64851f1a3feb97e086e25789393672d24467aed
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-06-08 14:11:25 -0700

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Finds the number of keys in a dictionary
+    Args:
+        a_dictionary: dictionary
+    Return: integer
+    """
+    return len(a_dictionary.keys())
