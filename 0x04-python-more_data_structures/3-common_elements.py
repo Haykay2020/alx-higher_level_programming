fe6615c779a9d739cee88c137ea6bb2b52305a1b
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-06-08 13:48:52 -0700

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    Creates a set of common elements in two sets
+    Args:
+        set_1: set
+        set_2: set
+    Return: set
+    """
+    return set_1 & set_2
